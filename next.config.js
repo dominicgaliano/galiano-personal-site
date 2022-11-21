863a7d20b110bfc8d64d483f6973761be68ebd31
Dominic Galiano <dgaliano@utexas.edu>
2022-11-20 22:08:39 -0600

    Attempting to configure non-static version of site to allow for image optimization

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    runtime: "experimental-edge",
+  },
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    unoptimized: true,
-  },
+  // images: {
+  //   unoptimized: true,
+  // },
 };
 
 module.exports = nextConfig;
