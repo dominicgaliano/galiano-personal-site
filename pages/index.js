ff43eb43ba69fdbb4e1f9a2709db5e3d9c7d487e
Dominic Galiano <dgaliano@utexas.edu>
2023-11-19 20:19:39 -0500

    Update dependencies and remove unused photos

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,6 @@ import { BsFillMoonStarsFill } from "react-icons/bs";
 import { BsLinkedin, BsGithub } from "react-icons/bs";
 import Image from "next/image";
 import dominicProfileImage from "../public/dag.png";
-import code from "../public/code.png";
-import design from "../public/design.png";
-import consulting from "../public/consulting.png";
-import web1 from "../public/web1.png";
-import web2 from "../public/web2.png";
-import web3 from "../public/web3.png";
-import web4 from "../public/web4.png";
-import web5 from "../public/web5.png";
-import web6 from "../public/web6.png";
 import { useState } from "react";
 
 export default function Home() {
